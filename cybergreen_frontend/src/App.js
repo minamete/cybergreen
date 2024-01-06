79872a20d0482f273ec973459d9eef15e439e3be
Jayne Bottarini <jebottarini@gmail.com>
2024-01-06 14:57:05 -0800

    added banner asset and homepage html/css

--- cybergreen_frontend/src/App.js
+++ cybergreen_frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Top Banner */}
+      <div className="banner">
+        <img src="CyberGreen.jpg" alt="CyberGreen Banner" />
+      </div>
+
+      {/* Navigation Menu */}
+      <nav className="menu">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Evaluate</a></li>
+          <li><a href="#">Leaderboard</a></li>
+        </ul>
+      </nav>
+
+      {/* Center Aligned Button */}
+      <div className="center-align">
+        <button className="evaluate-button">evaluate your idea</button>
+      </div>
     </div>
   );
 }
