b078dfd3c2b593080a37c80eca30566561040b42
lcaotemerity <andromefingercorn@gmail.com>
2024-01-07 14:28:14 -0500

    not sure if we need this

--- cybergreen_frontend/src/Leaderboard.js
+++ cybergreen_frontend/src/Leaderboard.js
@@ -3,7 +3,27 @@ import "./Leaderboard.css";
 import Table from "react-bootstrap/Table";
 
 const Leaderboard = () => {
+  useEffect(() => {
+    if (numberOfRowsOnPage === 0) return;
+
+    dbTablePages = [];
+    setCurrentPage(0);
+    let overallCt = 0;
+
+    for (let i = 0; i < dbTable.length / numberOfRowsOnPage; i++) {
+      dbTablePages.push([]);
+      for (let j = 0; j < numberOfRowsOnPage; j++) {
+        if (overallCt >= dbTable.length) return;
+        dbTablePages[i].push(dbTable[overallCt]);
+        overallCt++;
+      }
+    }
+  }, [numberOfRowsOnPage]);
+
   const [dbTable, setDBTable] = useState([]);
+  const [dbTablePages, setDBTablePages] = useState([]);
+  const [numberOfRowsOnPage, setNumberOfRowsOnPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     // load the rows from the db
@@ -21,8 +41,17 @@ const Leaderboard = () => {
         .then((data) => {
           // set table values and sort table
           setDBTable(
-            JSON.parse(data.response).sort((a, b) => (a.score > b.score ? -1 : 1))
+            JSON.parse(data.response)
+              .sort((a, b) => (a.score > b.score ? -1 : 1))
+              .map((e, i) => {
+                return {
+                  ...e,
+                  indx: i + 1,
+                };
+              })
           );
+
+          // this is to truncate the table when necessary
         });
     } catch (error) {
       console.error("DB loading failed", error);
@@ -43,12 +72,13 @@ const Leaderboard = () => {
             </tr>
           </thead>
           <tbody>
+            {}
             {dbTable.map((dbRow, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
+              <tr key={dbRow.indx}>
+                <td>{dbRow.indx}</td>
                 <td className="problem-cell">{dbRow.problem}</td>
                 <td className="solution-cell">{dbRow.solution}</td>
-                <td>{dbRow.score}</td>
+                <td className="score-cell">{dbRow.score}</td>
               </tr>
             ))}
           </tbody>
