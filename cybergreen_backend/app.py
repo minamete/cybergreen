8739bde9ee414d871fee246a82b5159180ec328d
Jayne Bottarini <jebottarini@gmail.com>
2024-01-06 21:33:40 -0800

    edited flask to add api endpoint

--- cybergreen_backend/app.py
+++ cybergreen_backend/app.py
@@ -1,6 +1,9 @@
-import chat
-from flask import Flask
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
+#import chat
+#from chat import get_openai_response
+import chat_fe
+from chat_fe import get_openai_response
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -14,5 +17,15 @@ def hello_world():
 @app.route("/chat")
 def test_chat(user_input):
     return get_openai_response(user_input)
+
+@app.route('/api/get_openai_response', methods=['POST'])
+def handle_openai_request():
+    print("entered flask")
+    user_input = request.json.get('user_input')
+    if user_input is not None:
+        openai_response = get_openai_response(user_input)
+        return jsonify(openai_response)
+    else:
+        return jsonify({'error': 'Invalid request'}), 400
 
 app.run()
