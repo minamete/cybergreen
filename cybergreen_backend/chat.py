877443ebb6e9555f59072b063fdffdb988c6c2af
Anastasiia Ilina <137015786+aii-anastasia@users.noreply.github.com>
2024-01-07 11:07:35 -0800

    Change # of tokens

--- cybergreen_backend/chat.py
+++ cybergreen_backend/chat.py
@@ -23,7 +23,7 @@ def get_openai_response(user_input):
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=1,
-        max_tokens=256,
+        max_tokens=4000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
